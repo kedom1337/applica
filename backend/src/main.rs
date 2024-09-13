1218e1f9190641148e86514eb50ac93d3a50e116
kedom1337 <kedom@vmcall.net>
2024-09-14 00:03:41 +0200

    feat: added tracing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,14 +3,36 @@ use axum::Router;
 mod routes;
 use routes::*;
 
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| {
+                    format!(
+                        "{}=debug,tower_http=debug,axum::rejection=trace",
+                        env!("CARGO_CRATE_NAME")
+                    )
+                    .into()
+                }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let api_v1_routes = Router::new()
         .nest("/application", application::get_router())
         .nest("/auth", auth::get_router());
 
-    let app = Router::new().nest("/api/v1", api_v1_routes);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let app_routes = Router::new()
+        .nest("/api/v1", api_v1_routes)
+        .layer(TraceLayer::new_for_http());
 
-    axum::serve(listener, app).await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app_routes).await.unwrap();
 }
