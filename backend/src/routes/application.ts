c77da586ed8777b2bdaf46f89d0d4c27fbbb119f
GoliathLabs <git@xdfr.de>
2024-10-18 22:41:27 -0400

    fix: define groups

--- backend/src/routes/application.ts
+++ backend/src/routes/application.ts
@@ -262,21 +262,25 @@ app.post('/status', zValidator('json', UpdateApplicationStatus), async (c) => {
                 }
               : userEntry
           )
+          // Definiere die Standardgruppen
+          const groups = ['cn=team,ou=services', 'cn=Mitglied,ou=group'];
+
           for (const groupDn of groups) {
             try {
               const change = new Change({
                 operation: 'add' as ChangeOperation,
                 modification: {
-                  memberUid: retryUid,
+                  memberUid: retryUid || uid,
                 },
               });
-          
+
               await ldap.modify(`${groupDn},${ldapConfig.baseDn}`, [change]);
               console.log(`User ${uid} added to group ${groupDn}`);
             } catch (error) {
               console.error(`Failed to add user ${uid} to group ${groupDn}:`, error);
             }
-          }          
+          }
+                
           console.info(`User ${cn} with password ${pass} added`)
           await sendEmail(rawApplication.email, uid, pass);
 
